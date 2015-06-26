91f12dda6077759a3e8e9a379986a3c02e7a4bb0
Klynton Jessup <klynton@puppetlabs.com>
2015-06-26 12:58:53 -0700

    Update the Wordpress version.
    Updated the version of Wordpress from 3.8 to 4.2.2
    and added a variable to make future updates easier.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,14 +9,16 @@ class training {
     source  => '/usr/src/puppetlabs-training-bootstrap/scripts/classroom',
   }
 
+  $wordpress_version = '4.2.2'
+
   file { '/usr/src/wordpress':
     ensure => directory,
   }
 
   exec { 'Cache WordPress':
     cwd       => '/usr/src/wordpress',
-    command   => '/usr/bin/wget --no-clobber --no-check-certificate https://www.wordpress.org/wordpress-3.8.tar.gz',
-    creates   => '/usr/src/wordpress/wordpress-3.8.tar.gz',
+    command   => "/usr/bin/wget --no-clobber --no-check-certificate https://www.wordpress.org/wordpress-${wordpress_version}.tar.gz",
+    creates   => "/usr/src/wordpress/wordpress-${wordpress_version}.tar.gz",
     logoutput => 'on_failure',
     user      => 'root',
     group     => 'root',
